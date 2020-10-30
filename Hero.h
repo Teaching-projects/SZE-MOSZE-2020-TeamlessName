ce48e8331c5107970bfd4dce5a17290a4ef8a47c
eriklorincz <44705877+eriklorincz@users.noreply.github.com>
2020-10-30 10:20:40 +0100

    Fix warning, reordered variables

--- Hero.h
+++ Hero.h
@@ -4,13 +4,16 @@
 class Hero : public Monster
 {
 private:
-	int XP; //The amount of XP that is not used yet for level-up
+	
 	int XPgap; //The amount of xp that is needed for a level-up
-	int Lvl = 1;
-
 	int HPbonus;
 	int DMGbonus;
 	float CDMultiplier;
+	
+	int XP; //The amount of XP that is not used yet for level-up
+	int Lvl = 1;
+
+
 	void levelUp();
 public:
 	Hero(const std::string& nm, int hp, int dmg,float as, int xpgap, int hpbonus, int dmgbonus, float cdMulti);
