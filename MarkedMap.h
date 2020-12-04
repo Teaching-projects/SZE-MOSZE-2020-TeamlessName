9609391a2fbe68d1467ee27655c393f4234b1d88
kalmar-adam <kalmar.adam94@gmail.com>
2020-12-04 10:46:21 +0100

    Add MarkedMap class and mmap.txt

---
+++ MarkedMap.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Map.h"
+#include <string>
+#include <fstream>
+#include <list>
+#include <vector>
+/**
+ * \class MarkedMap
+ *
+ * \brief MarkedMap class for loading and handling a map from a text file. Inherited from Map class
+ *
+ */
+class MarkedMap: Map
+{
+    public:
+    MarkedMap(){}; ///< Default (empty constructor of MarkedMap)
+    /**
+    * \brief MarkedMap constructor for load the map from a text file
+    *
+    * \param An existing (map) text file
+    *
+    */
+    MarkedMap(std::string fname);
+    /**
+    * \brief Search the Hero coordinate on the map
+    *
+    * \return A single pair of integer coordinate of the Hero
+    */
+    std::pair<int,int> getHeroPosition() const;
+    /**
+    * \brief Search and collect the monsters/mobs coordinates on the map
+    *
+    * \return A vector that stores pairs of monsters (integers) coordinates
+    */
+    std::vector<std::pair<int,int>> getMonsterPositions(char c) const;
+
+};
