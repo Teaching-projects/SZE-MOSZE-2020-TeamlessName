0e7b54b5964b7a0a54922a6e3fe0fbae8f0192f7
kalmar-adam <63585863+kalmar-adam@users.noreply.github.com>
2020-12-08 16:07:43 +0000

    Update gh-pages to output generated at 00cdb27

--- search/all_4.js
+++ search/all_4.js
@@ -4,10 +4,10 @@ var searchData=
   ['gamealreadystartedexception_11',['GameAlreadyStartedException',['../class_game_1_1_game_already_started_exception.html',1,'Game']]],
   ['get_12',['get',['../class_j_s_o_n.html#aebfe4eae3134bdc249f9695c6406bdba',1,'JSON::get()'],['../class_map.html#af3d2f71301bb5bdafcef6099a65ae7e0',1,'Map::get()']]],
   ['getattackcooldown_13',['getAttackCoolDown',['../class_hero.html#af76e70bb236599e7a53096f2cd5e193e',1,'Hero']]],
-  ['getheight_14',['getHeight',['../class_map.html#a67113f1ffac80d9a20b15fa18a58edc2',1,'Map']]],
+  ['getheight_14',['getHeight',['../class_map.html#ad9488e4177a474442d53ad2ec0f0ca30',1,'Map']]],
   ['getheroposition_15',['getHeroPosition',['../class_marked_map.html#aaa25768bfdd3ea4b21e847882a3f9ccd',1,'MarkedMap']]],
   ['getmaxhealthpoints_16',['getMaxHealthPoints',['../class_hero.html#ac6a7deeb6e390a23536d622042f4c48e',1,'Hero']]],
   ['getmonsterpositions_17',['getMonsterPositions',['../class_marked_map.html#a5e533e861edffc4b352219c5f573afd2',1,'MarkedMap']]],
-  ['getwidth_18',['getWidth',['../class_map.html#ab3181204ba96eb15b96ee1ced85e198e',1,'Map']]],
+  ['getwidth_18',['getWidth',['../class_map.html#a7487aeefaf5622ede3e4c5c1f99535ef',1,'Map']]],
   ['gothit_19',['gotHit',['../class_monster.html#ac73043e8c28dd6e6627549987cae315b',1,'Monster']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hero_90',['Hero',['../class_hero.html#aefc098f8138ab7e7d75ac7c5ba1cf7df',1,'Hero::Hero(const std::string &amp;nm, int hp, int pdmg, int mdmg, double cd, int df, int xpgap, int hpbonus, int dmgbonus, int mdamagebonus, double cdMulti, int dfbonus)'],['../class_hero.html#addf4f57ee4e49a8cf5f43681cb31daa1',1,'Hero::Hero(const Hero &amp;)']]]
+  ['hero_90',['Hero',['../class_hero.html#acbcb48f080d42e7eb9b7700b02eb16c1',1,'Hero::Hero(const std::string &amp;nm, int hp, int pdmg, int mdmg, double cd, int df, int xpgap, int hpbonus, int dmgbonus, int mdamagebonus, double cdMulti, int dfbonus, unsigned int lr, unsigned int lrbonus)'],['../class_hero.html#addf4f57ee4e49a8cf5f43681cb31daa1',1,'Hero::Hero(const Hero &amp;)']]]
 ];
